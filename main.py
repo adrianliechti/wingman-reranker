b0f35666bc5912ed42b568f89699604c6b5c685a
Adrian Liechti <adrian@monobox.ch>
2024-09-15 13:01:36 +0200

    simple reranker

---
+++ main.py
@@ -0,0 +1,53 @@
+import torch
+import uvicorn
+
+from fastapi import FastAPI
+from typing import List, Optional
+from pydantic import BaseModel
+
+from transformers import AutoModelForSequenceClassification
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+model = AutoModelForSequenceClassification.from_pretrained(
+    'jinaai/jina-reranker-v2-base-multilingual',
+    torch_dtype="auto",
+    trust_remote_code=True,
+)
+
+model.to(device)
+model.eval()
+
+app = FastAPI(
+    title="LLM Platform Tools"
+)
+
+class RerankRequest(BaseModel):
+    query: str
+    documents: List[str]
+
+    top_n: Optional[int] = None
+
+@app.post("/rerank")
+async def rerank(request: RerankRequest):
+    query = request.query
+    documents = request.documents
+    
+    top_n = request.top_n
+
+    pairs = [[query, doc] for doc in documents]
+    scores = model.compute_score(pairs, max_length=1024)
+
+    results = [{"index": i, "document": { "text": doc }, "relevance_score": score} for i, (doc, score) in enumerate(zip(documents, scores))]
+    results = sorted(results, key=lambda x: x['relevance_score'], reverse=True)
+    
+    if top_n is not None:
+        results = results[:top_n]
+
+    return {
+        "model": model.name_or_path,
+        "results": results
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
